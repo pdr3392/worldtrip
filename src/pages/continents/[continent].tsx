b02a2901b127d7ce285de8b7c7dd6cfcd47e9ec1
pdr3392 <pchenr1que1@gmail.com>
2022-01-04 08:27:28 -0300

    adding head title

--- src/pages/continents/[continent].tsx
+++ src/pages/continents/[continent].tsx
@@ -9,6 +9,7 @@ import {
 } from "@chakra-ui/react";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { Params } from "next/dist/server/router";
+import Head from "next/head";
 import DestinationLargeInfo from "../../components/DestinationLargeInfo";
 import DestinationMainInfo from "../../components/DestinationMainInfo";
 import Header from "../../components/Header";
@@ -44,107 +45,113 @@ interface CurrentContinentProps {
 
 export default function Continent({ continent }: CurrentContinentProps) {
   return (
-    <Flex
-      bgColor="gray.200"
-      direction="column"
-      align="center"
-      maxWidth="100vw"
-      overflow="hidden"
-    >
-      <Header needBackward />
+    <>
+      <Head>
+        <title>WorldTrip | {continent.name}</title>
+      </Head>
 
       <Flex
+        bgColor="gray.200"
         direction="column"
-        maxWidth={{ base: 425, sm: 767, md: 1023, lg: 1439, xl: 1440 }}
-        w="100vw"
+        align="center"
+        maxWidth="100vw"
+        overflow="hidden"
       >
-        <Box
-          position="relative"
-          as="div"
+        <Header needBackward />
+
+        <Flex
+          direction="column"
           maxWidth={{ base: 425, sm: 767, md: 1023, lg: 1439, xl: 1440 }}
-          maxHeight={{ base: 150, sm: 250, md: 500 }}
-          overflow="hidden"
-          mb={{ base: "5", sm: "10", md: "8", lg: "10", xl: "20" }}
+          w="100vw"
         >
-          <Text
-            margin={{
-              base: "4rem auto auto 6.5rem",
-              sm: "6rem auto auto 10.1rem",
-              md: "7rem auto auto 10.5rem",
-              lg: "23rem auto 3.75rem 5rem",
-              xl: "23rem auto 3.75rem 8.5rem",
+          <Box
+            position="relative"
+            as="div"
+            maxWidth={{ base: 425, sm: 767, md: 1023, lg: 1439, xl: 1440 }}
+            maxHeight={{ base: 150, sm: 250, md: 500 }}
+            overflow="hidden"
+            mb={{ base: "5", sm: "10", md: "8", lg: "10", xl: "20" }}
+          >
+            <Text
+              margin={{
+                base: "4rem auto auto 6.5rem",
+                sm: "6rem auto auto 10.1rem",
+                md: "7rem auto auto 10.5rem",
+                lg: "23rem auto 3.75rem 5rem",
+                xl: "23rem auto 3.75rem 8.5rem",
+              }}
+              position="absolute"
+              fontFamily="Poppins"
+              fontWeight="600"
+              fontSize={["1.75rem", "3rem"]}
+              lineHeight={["2.625rem", "4.5rem"]}
+              color="gray.200"
+              textShadow="7px -4px 6px rgba(0,0,0,0.92)"
+            >
+              {continent.name}
+            </Text>
+            <Image src={continent.continentBanner} alt="Banner" />
+          </Box>
+
+          <DestinationMainInfo
+            mainInfo={continent.lgInfo}
+            lgInfo={continent.lgItems}
+          />
+
+          <Box
+            ml={{
+              base: "2rem",
+              sm: "6.5rem",
+              md: "6.5rem",
+              lg: "5rem",
+              xl: "8.75rem",
             }}
-            position="absolute"
-            fontFamily="Poppins"
-            fontWeight="600"
-            fontSize={["1.75rem", "3rem"]}
-            lineHeight={["2.625rem", "4.5rem"]}
-            color="gray.200"
-            textShadow="7px -4px 6px rgba(0,0,0,0.92)"
+            maxWidth={{ base: "375px", lg: "975px", xl: "1160px" }}
           >
-            {continent.name}
-          </Text>
-          <Image src={continent.continentBanner} alt="Banner" />
-        </Box>
+            <Text
+              mt={["5", "20"]}
+              fontWeight="500"
+              mb={["1.25rem", "10"]}
+              color="green.900"
+              fontSize={["1.5rem", "2.25rem"]}
+              lineHeight="3.375rem"
+            >
+              Cidades +100
+            </Text>
+          </Box>
 
-        <DestinationMainInfo
-          mainInfo={continent.lgInfo}
-          lgInfo={continent.lgItems}
-        />
-
-        <Box
-          ml={{
-            base: "2rem",
-            sm: "6.5rem",
-            md: "6.5rem",
-            lg: "5rem",
-            xl: "8.75rem",
-          }}
-          maxWidth={{ base: "375px", lg: "975px", xl: "1160px" }}
-        >
-          <Text
-            mt={["5", "20"]}
-            fontWeight="500"
-            mb={["1.25rem", "10"]}
-            color="green.900"
-            fontSize={["1.5rem", "2.25rem"]}
-            lineHeight="3.375rem"
+          <Box
+            mt={["0.25rem", "0.5rem"]}
+            width="100%"
+            direction={["row", "column"]}
+            maxWidth={{
+              base: "256px",
+              md: "560px",
+              lg: "860px",
+              xl: "1160px",
+            }}
+            ml={{ base: "auto", lg: "5rem", xl: "8.5rem" }}
+            mr={{ base: "auto", lg: "auto", xl: "auto" }}
+            mb={["2", "2rem"]}
           >
-            Cidades +100
-          </Text>
-        </Box>
-
-        <Box
-          mt={["0.25rem", "0.5rem"]}
-          width="100%"
-          direction={["row", "column"]}
-          maxWidth={{
-            base: "256px",
-            md: "560px",
-            lg: "860px",
-            xl: "1160px",
-          }}
-          ml={{ base: "auto", lg: "5rem", xl: "8.5rem" }}
-          mr={{ base: "auto", lg: "auto", xl: "auto" }}
-          mb={["2", "2rem"]}
-        >
-          <Wrap spacing={["0", "2.813rem"]}>
-            {continent.hundredCities.map((city) => (
-              <WrapItem key={city.city}>
-                <Center>
-                  <DestinationLargeInfo
-                    city={city.city}
-                    country={city.country}
-                    folderPath={city.folderPath}
-                    elipseCountry={city.elipseCountry}
-                  />
-                </Center>
-              </WrapItem>
-            ))}
-          </Wrap>
-        </Box>
+            <Wrap spacing={["0", "2.813rem"]}>
+              {continent.hundredCities.map((city) => (
+                <WrapItem key={city.city}>
+                  <Center>
+                    <DestinationLargeInfo
+                      city={city.city}
+                      country={city.country}
+                      folderPath={city.folderPath}
+                      elipseCountry={city.elipseCountry}
+                    />
+                  </Center>
+                </WrapItem>
+              ))}
+            </Wrap>
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import {
   WrapItem,
 } from "@chakra-ui/react";
 import { GetStaticProps } from "next";
+import Head from "next/head";
 import BodyIcon from "../components/BodyIcon";
 import Header from "../components/Header";
 import SwiperCarousel from "../components/SwiperCarousel";
@@ -50,122 +51,134 @@ export default function Home({ continents }: HomeProps) {
   });
 
   return (
-    <Flex
-      direction="column"
-      bgColor="gray.200"
-      align="center"
-      maxWidth={{ base: "100vw", xl: "100%" }}
-      overflow="hidden"
-    >
-      <Header needBackward={false} />
+    <>
+      <Head>
+        <title>WorldTrip | Home</title>
+      </Head>
 
       <Flex
         direction="column"
+        bgColor="gray.200"
         align="center"
-        maxWidth={{ base: 479, sm: 767, md: 1023, lg: 1439, xl: 1440 }}
+        maxWidth={{ base: "100vw", xl: "100%" }}
+        overflow="hidden"
       >
-        <Box
-          w="100%"
-          align="center"
-          mb={{ base: "5", sm: "12", md: "14", lg: "20", xl: "20" }}
-        >
-          {isWideVersion ? (
-            <Image w="100vw" src="/images/Banner.svg" alt="Banner" />
-          ) : (
-            <Image w="100%" src="/images/mobileBanner.svg" alt="Banner" />
-          )}
-        </Box>
+        <Header needBackward={false} />
 
-        <Flex w="100%" justify="center">
-          <Wrap
-            justify="center"
-            w={{ base: "85%", sm: "100%", md: "100%", lg: "100%", xl: "100%" }}
-            spacing={{ base: "7", sm: "16", md: "14", lg: "32", xl: "32" }}
-            direction="row"
-            m={{ base: "auto", sm: "7", md: "8", lg: "35", xl: "35" }}
-            mb={{ base: "12", md: "16", lg: "32", xl: "32" }}
-          >
-            <WrapItem>
-              <Center w="136px" h="21px">
-                <BodyIcon filename="cocktail" info="vida noturna" />
-              </Center>
-            </WrapItem>
-            <WrapItem>
-              <Center w="136px" h="21px">
-                <BodyIcon filename="surf" info="praia" />
-              </Center>
-            </WrapItem>
-            <WrapItem>
-              <Center w="136px" h="21px">
-                <BodyIcon filename="building" info="moderno" />
-              </Center>
-            </WrapItem>
-            <WrapItem>
-              <Center w="136px" h="21px">
-                <BodyIcon filename="museum" info="clássico" />
-              </Center>
-            </WrapItem>
-            <WrapItem>
-              <Center w="136px" h="21px">
-                <BodyIcon filename="earth" info="e mais..." />
-              </Center>
-            </WrapItem>
-          </Wrap>
-        </Flex>
-
-        <Stack
-          spacing={{ base: "1.25rem", md: "8", lg: "10", xl: "14" }}
+        <Flex
+          direction="column"
           align="center"
-          mb={{ base: "3", md: "4", lg: "6", xl: "10" }}
+          maxWidth={{ base: 479, sm: 767, md: 1023, lg: 1439, xl: 1440 }}
         >
           <Box
-            w={{ base: "60px", md: "70px", lg: "90px", xl: "90px" }}
-            border={{
-              base: "1px solid rgba(71, 88, 91, 0.8)",
-              md: "1px solid #47585B",
-              lg: "2px solid #47585B",
-              xl: "2px solid #47585B",
-            }}
-          />
+            w="100%"
+            align="center"
+            mb={{ base: "5", sm: "12", md: "14", lg: "20", xl: "20" }}
+          >
+            {isWideVersion ? (
+              <Image w="100vw" src="/images/Banner.svg" alt="Banner" />
+            ) : (
+              <Image w="100%" src="/images/mobileBanner.svg" alt="Banner" />
+            )}
+          </Box>
 
-          <Stack as="div" align="center">
-            <Text
-              fontSize={{
-                base: "1.25rem",
-                md: "1.6rem",
-                lg: "1.85rem",
-                xl: "2.25rem",
+          <Flex w="100%" justify="center">
+            <Wrap
+              justify="center"
+              w={{
+                base: "85%",
+                sm: "100%",
+                md: "100%",
+                lg: "100%",
+                xl: "100%",
               }}
-              fontWeight="500"
-              lineHeight={{
-                base: "1.875rem",
-                md: "1.875rem",
-                lg: "1.25rem",
-                xl: "1",
-              }}
-              color="gray.900"
+              spacing={{ base: "7", sm: "16", md: "14", lg: "32", xl: "32" }}
+              direction="row"
+              m={{ base: "auto", sm: "7", md: "8", lg: "35", xl: "35" }}
+              mb={{ base: "12", md: "16", lg: "32", xl: "32" }}
             >
-              Vamos nessa?
-            </Text>
-            <Text
-              fontSize={{
-                base: "1.25rem",
-                md: "1.6rem",
-                lg: "2rem",
-                xl: "2.25rem",
+              <WrapItem>
+                <Center w="136px" h="21px">
+                  <BodyIcon filename="cocktail" info="vida noturna" />
+                </Center>
+              </WrapItem>
+              <WrapItem>
+                <Center w="136px" h="21px">
+                  <BodyIcon filename="surf" info="praia" />
+                </Center>
+              </WrapItem>
+              <WrapItem>
+                <Center w="136px" h="21px">
+                  <BodyIcon filename="building" info="moderno" />
+                </Center>
+              </WrapItem>
+              <WrapItem>
+                <Center w="136px" h="21px">
+                  <BodyIcon filename="museum" info="clássico" />
+                </Center>
+              </WrapItem>
+              <WrapItem>
+                <Center w="136px" h="21px">
+                  <BodyIcon filename="earth" info="e mais..." />
+                </Center>
+              </WrapItem>
+            </Wrap>
+          </Flex>
+
+          <Stack
+            spacing={{ base: "1.25rem", md: "8", lg: "10", xl: "14" }}
+            align="center"
+            mb={{ base: "3", md: "4", lg: "6", xl: "10" }}
+          >
+            <Box
+              w={{ base: "60px", md: "70px", lg: "90px", xl: "90px" }}
+              border={{
+                base: "1px solid rgba(71, 88, 91, 0.8)",
+                md: "1px solid #47585B",
+                lg: "2px solid #47585B",
+                xl: "2px solid #47585B",
               }}
-              fontWeight="500"
-              lineHeight={{ base: "1.875rem", md: "2", lg: "2", xl: "2" }}
-              color="gray.900"
-            >
-              Então escolha seu continente
-            </Text>
+            />
+
+            <Stack as="div" align="center">
+              <Text
+                fontSize={{
+                  base: "1.25rem",
+                  md: "1.6rem",
+                  lg: "1.85rem",
+                  xl: "2.25rem",
+                }}
+                fontWeight="500"
+                lineHeight={{
+                  base: "1.875rem",
+                  md: "1.875rem",
+                  lg: "1.25rem",
+                  xl: "1",
+                }}
+                color="gray.900"
+              >
+                Vamos nessa?
+              </Text>
+              <Text
+                fontSize={{
+                  base: "1.25rem",
+                  md: "1.6rem",
+                  lg: "2rem",
+                  xl: "2.25rem",
+                }}
+                fontWeight="500"
+                lineHeight={{ base: "1.875rem", md: "2", lg: "2", xl: "2" }}
+                color="gray.900"
+              >
+                Então escolha seu continente
+              </Text>
+            </Stack>
+
+            <SwiperCarousel continents={continents} />
           </Stack>
-
-          <SwiperCarousel continents={continents} />
-        </Stack>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
 
